555142912a10d2bef6de670469e5d9ff62b8a663
Pink Elephant <manashaperera1995@gmail.com>
2018-08-05 00:48:29 +0530

    Created Front ends

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title:'crudapp'});
+});
+
+router.get('/addUsers', function(req, res, next) {
+  res.render('adduserform', { title:'crudapp'});
+});
+
+router.get('/usersTable', function(req, res, next) {
+  res.render('usersTable', { title:'crudapp'});
 });
 
 module.exports = router;
